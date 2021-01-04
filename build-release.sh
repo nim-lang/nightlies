b3ea52b4d8ff81ce417ad4ac90f8d3ce7fcd9bff
Leorize <leorize+oss@disroot.org>
2021-01-04 12:23:34 -0600

    build-release: parse version using regex
    While `nim secret` was useful for obtaining this information
    programmatically, its output appears to not be as stable as expected.
    
    This commit change build-release to use bash regex capability to obtain
    the needed information from `nim --version`.

--- build-release.sh
+++ build-release.sh
@@ -128,12 +128,11 @@ time {
   endfold
 }
 
-eval "$(cat << EOF | nim secret --hints:off 2>/dev/null
-echo "version=", NimVersion
-echo "os=", hostOS
-quit 0
-EOF
-)"
+versionRegex='^Nim Compiler Version ([0-9]+\.[0-9]+\.[0-9]+) \[([a-zA-Z0-9]+): [a-zA-Z0-9]+\]'
+if [[ $(nim --version 2>&1) =~ $versionRegex ]]; then
+  version=${BASH_REMATCH[1]}
+  os=${BASH_REMATCH[2],,}
+fi
 
 # Fail if the variables are not declared (ie. nim couldn't run)
 : "${version:?}" "${os:?}"
