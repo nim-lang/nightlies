dffaa1cc5bc3ce51725cbdfee9224ad5d3bdc26f
Jaremy Creechley <creechley@gmail.com>
2025-01-29 01:10:50 -0700

    update libdir install pcre2

--- deps.sh
+++ deps.sh
@@ -106,7 +106,7 @@ if [[ ! -d $output ]]; then
       xargs < "$basedir/buildreq.txt" "$basedir/bw-install.sh" -o "$toolchain" -t "$triple"
       ;;
     darwin)
-      brew install coreutils
+      brew install coreutils sqlite pcre2
       xargs < "$basedir/buildreq.txt" "$basedir/bw-install.sh" -t "$triple"
       ;;
   esac
